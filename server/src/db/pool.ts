e8fd762bac7f2bc29bb0ea3448bc3f6294227038
Aryan Parmar <156536547+notaarryan@users.noreply.github.com>
2026-07-15 14:01:43 -0400

    fix: fixed pool error

--- server/src/db/pool.ts
+++ server/src/db/pool.ts
@@ -1,10 +1,6 @@
 import "dotenv/config";
 import { Pool } from "pg";
 const pool = new Pool({
-  port: Number(process.env.POSTGRESS_PORT) || 5432,
-  user: process.env.POSTGRESS_USERNAME!,
-  database: process.env.POSTGRESS_DB!,
-  password: process.env.POSTGRESS_PASSWORD!,
-  host: process.env.POSTGRESS_HOST!,
+  connectionString: process.env.DATABASE_URL,
 });
 export default pool;
